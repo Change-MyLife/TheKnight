803b8e0c6fabf72d2c1bc1b2134f47a307544b89
SMW <tjalsdn5869@yoyointeractive.com>
2025-02-21 18:17:45 +0900

    2025-02-21 서민우 캐릭터 기본 움직임 구현

---
+++ Assets/0.TheKnight/0.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+
+    Vector2 dir;
+    public float speed = 5;
+
+    private void Awake()
+    {
+        rb = transform.GetComponent<Rigidbody2D>();
+        anim = transform.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        SetAnim();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if(horizontal == 0 && vertical == 0)
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+
+        dir = new Vector2(horizontal, vertical);
+        rb.linearVelocity = dir * speed * Time.fixedDeltaTime;
+    }
+
+    void SetAnim()
+    {
+        anim.SetFloat("MoveX", dir.x);
+        anim.SetFloat("MoveY", dir.y);
+    }
+}
